2cb1f0ed491f78941fb807a6a948b92827300ae7
dan-vds <danielvds101@gmail.com>
2025-06-22 20:57:04 +1200

    Changed base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/lisavds.github.io/',
+  base: '/',
   build: {
     assetsDir: 'assets',
     rollupOptions: {
